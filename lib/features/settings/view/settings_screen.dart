a892f4cd38eb5759f05cce76c28cc1223e7bcce3
QUSAD.prod <qusad.prod@gmail.com>
2025-03-12 10:25:09 +0300

    add Settings Page

--- lib/features/settings/view/settings_screen.dart
+++ lib/features/settings/view/settings_screen.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:messenger_demo/core/strings/hive_strings.dart';
 
 @RoutePage()
 class SettingsScreen extends StatefulWidget {
@@ -13,8 +15,59 @@ class _SettingsScreenState extends State<SettingsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(),
-      body: Container(),
+      appBar: AppBar(
+        title: Text('Settings'),
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: Hive.box(HiveStrings.settingsBoxName).listenable(),
+        builder: (context, box, widget) {
+          return SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: [
+                  _themeSwitch(context: context, box: box),
+                  Divider(),
+                  Divider(),
+                  Text("Version: ${box.get(HiveStrings.versionPath)}"),
+                  Text("Build: ${box.get(HiveStrings.buildNumberPath) == "" ? "-" : box.get(HiveStrings.buildNumberPath)}"),
+                  Text("by QUSAD.prod"),
+                ],
+              ),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget _themeSwitch({required BuildContext context, required Box box}) {
+    String currentValue = box.get(HiveStrings.themePath, defaultValue: HiveStrings.themeSystem);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.only(left: 16.0, top: 8.0),
+          child: Text("Theme mode"),
+        ),
+        RadioListTile(
+          title: Text("Light"),
+          value: HiveStrings.themeLight,
+          groupValue: currentValue,
+          onChanged: (value) => box.put(HiveStrings.themePath, HiveStrings.themeLight),
+        ),
+        RadioListTile(
+          title: Text("Dark"),
+          value: HiveStrings.themeDark,
+          groupValue: currentValue,
+          onChanged: (value) => box.put(HiveStrings.themePath, HiveStrings.themeDark),
+        ),
+        RadioListTile(
+          title: Text("System"),
+          value: HiveStrings.themeSystem,
+          groupValue: currentValue,
+          onChanged: (value) => box.put(HiveStrings.themePath, HiveStrings.themeSystem),
+        ),
+      ],
     );
   }
 }
