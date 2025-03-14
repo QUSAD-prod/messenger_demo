984e27a554f1307573f4a982e59e2d515e18e8e7
QUSAD.prod <qusad.prod@gmail.com>
2025-03-14 17:49:01 +0300

    add basic color theme and fonts

---
+++ lib/core/theme/colors.dart
@@ -0,0 +1,39 @@
+import 'dart:ui';
+
+class AppColors {
+  // Светлая тема
+  static const Color lightBackground = Color(0xFFFFFFFF);
+  static const Color lightAppBarBackground = Color(0xFFFFFFFF);
+  static const Color lightChatBackground = Color(0xFFFFFFFF);
+  static const Color lightPrimaryText = Color(0xFF000000);
+  static const Color lightSecondaryText = Color(0xFF757575);
+  static const Color lightIncomingMessageText = Color(0xFF000000);
+  static const Color lightOutgoingMessageText = Color(0xFFFFFFFF);
+  static const Color lightIncomingMessageBackground = Color(0xFFFFFFFF);
+  static const Color lightPrimaryColor = Color(0xFF14C550);
+  static const Color lightIconColor = Color(0xFF757575);
+  static const Color lightButtonColor = Color(0xFF128C7E);
+  static const Color lightDividerColor = Color(0xFFE0E0E0);
+  static const Color lightOnlineStatus = Color(0xFF4CAF50);
+  static const Color lightOfflineStatus = Color(0xFF757575);
+  static const Color lightNotificationBackground = Color(0xFFFFEB3B);
+  static const Color lightNotificationText = Color(0xFF000000);
+
+  // Темная тема
+  static const Color darkBackground = Color(0xFF121212);
+  static const Color darkAppBarBackground = Color(0xFF1F1F1F);
+  static const Color darkChatBackground = Color(0xFF1E1E1E);
+  static const Color darkPrimaryText = Color(0xFFFFFFFF);
+  static const Color darkSecondaryText = Color(0xFFBDBDBD);
+  static const Color darkIncomingMessageText = Color(0xFFFFFFFF);
+  static const Color darkOutgoingMessageText = Color(0xFF000000);
+  static const Color darkIncomingMessageBackground = Color(0xFF2D2D2D);
+  static const Color darkPrimaryColor = Color(0xFF00B13C);
+  static const Color darkIconColor = Color(0xFFBDBDBD);
+  static const Color darkButtonColor = Color(0xFF128C7E);
+  static const Color darkDividerColor = Color(0xFF333333);
+  static const Color darkOnlineStatus = Color(0xFF4CAF50);
+  static const Color darkOfflineStatus = Color(0xFF757575);
+  static const Color darkNotificationBackground = Color(0xFFFFEB3B);
+  static const Color darkNotificationText = Color(0xFF000000);
+}
--- lib/core/theme/theme.dart
+++ lib/core/theme/theme.dart
@@ -1,13 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:messenger_demo/core/strings/hive_strings.dart';
+import 'package:messenger_demo/core/theme/colors.dart';
 
 class GlobalTheme {
   static final ThemeData lightTheme = ThemeData(
     useMaterial3: true,
     brightness: Brightness.light,
     splashFactory: InkRipple.splashFactory,
+    fontFamily: 'Gilroy',
+    primaryColor: AppColors.lightAppBarBackground,
+    scaffoldBackgroundColor: AppColors.lightBackground,
     appBarTheme: AppBarTheme(
-      centerTitle: true,
+      backgroundColor: AppColors.lightAppBarBackground,
+      foregroundColor: AppColors.lightPrimaryText,
+    ),
+    textTheme: TextTheme(
+      bodyLarge: TextStyle(color: AppColors.lightPrimaryText),
+      bodyMedium: TextStyle(color: AppColors.lightPrimaryText),
+    ),
+    colorScheme: ColorScheme.light(
+      primary: AppColors.lightPrimaryColor,
+      secondary: AppColors.lightSecondaryText,
+      surface: AppColors.lightChatBackground,
     ),
   );
 
@@ -15,8 +29,21 @@ class GlobalTheme {
     useMaterial3: true,
     brightness: Brightness.dark,
     splashFactory: InkRipple.splashFactory,
+    fontFamily: 'Gilroy',
+    primaryColor: AppColors.darkAppBarBackground,
+    scaffoldBackgroundColor: AppColors.darkBackground,
     appBarTheme: AppBarTheme(
-      centerTitle: true,
+      backgroundColor: AppColors.darkAppBarBackground,
+      foregroundColor: AppColors.darkPrimaryText,
+    ),
+    textTheme: TextTheme(
+      bodyLarge: TextStyle(color: AppColors.darkPrimaryText),
+      bodyMedium: TextStyle(color: AppColors.darkPrimaryText),
+    ),
+    colorScheme: ColorScheme.dark(
+      primary: AppColors.darkPrimaryColor,
+      secondary: AppColors.lightSecondaryText,
+      surface: AppColors.darkChatBackground,
     ),
   );
 
